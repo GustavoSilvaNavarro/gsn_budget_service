77feca9de08d56479ce48e16a1e77ba216816bfa
Gustavo Silva <gustavo.silva.nav@gmail.com>
2025-11-02 20:35:44 -0600

    ⚡️ Final log setup has been fully setup

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,9 +1,11 @@
 package logger
 
 import (
+	"fmt"
 	"os"
 	"runtime/debug"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/gsn_budget_service/internal/config"
@@ -11,28 +13,45 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
+var once sync.Once
+
 func InitLogger() {
-	buildInfo, _ := debug.ReadBuildInfo()
-	zerolog.TimeFieldFormat = time.RFC3339
+	once.Do(func() {
+		buildInfo, _ := debug.ReadBuildInfo()
+		zerolog.TimeFieldFormat = time.RFC3339
 
-	logLevel := strings.ToLower(config.Cfg.LOG_LEVEL)
-	envLevel := strings.ToLower(config.Cfg.ENVIRONMENT)
+		logLevel := strings.ToLower(config.Cfg.LOG_LEVEL)
+		envLevel := strings.ToLower(config.Cfg.ENVIRONMENT)
 
-	level, err := zerolog.ParseLevel(logLevel)
-	if err != nil {
-		level = zerolog.DebugLevel // in case of error we default to debug
-	}
+		level, err := zerolog.ParseLevel(logLevel)
+		if err != nil {
+			level = zerolog.DebugLevel // in case of error we default to debug
+		}
 
-	zerolog.SetGlobalLevel(level)
+		zerolog.SetGlobalLevel(level)
+		output := os.Stdout
+		writer := zerolog.New(output)
 
-	log.Logger = log.Logger.With().
-		Caller(). // Caller (file and line number) to every log event
-		Str("env", envLevel).
-		Str("service", config.Cfg.NAME).
-		Int("pid", os.Getpid()).
-		Str("lang", "Golang").
-		Str("go_version", buildInfo.GoVersion).
-		Logger()
+		if envLevel != "stg" && envLevel != "prd" && envLevel != "dev" {
+			writer = zerolog.New(zerolog.ConsoleWriter{
+				Out:        output,
+				TimeFormat: time.RFC3339,
+				FormatMessage: func(i any) string {
+					return fmt.Sprintf("| %s |", i)
+				},
+			})
+		}
 
-	log.Info().Msg("🪵 Logger initialized!")
+		log.Logger = writer.With().
+			Timestamp().
+			Caller(). // Caller (file and line number) to every log event
+			Str("env", envLevel).
+			Str("service", config.Cfg.NAME).
+			Int("pid", os.Getpid()).
+			Str("lang", "Golang").
+			Str("go_version", buildInfo.GoVersion).
+			Logger()
+
+		log.Error().Msg("🪵 Logger initialized!")
+	})
 }
